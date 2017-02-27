0764fd6d8728223d6de7b2a829416479d9487e95
faebug <faebug@gmail.com>
2017-02-27 15:52:46 +0000

    Add FAA & FSA

--- relicense_PD-USGov.py
+++ relicense_PD-USGov.py
@@ -25,11 +25,16 @@ print Fore.YELLOW + NOTICE, Fore.WHITE
 
 site = pywikibot.getSite('commons', 'commons')
 searchstrings = [
-	['insource:/= Environmental Protection Agency/', 'PD-USGov-EPA'],
 	['insource:/fema.gov|Federal Emergency Management Agency/', 'PD-USGov-FEMA'],
+	['insource:/www.faa.gov|PD-USGov-FAA/','PD-USGov-FAA'],
+	['insource:/PD-USGov-FSA/','PD-USGov-FSA'],
+	['insource:/arm.gov|PD-USGov-ARM/','PD-USGov-ARM'],
+	['insource:/= Environmental Protection Agency/', 'PD-USGov-EPA'],
 	['insource:/nasa.gov/', 'PD-USGov-NASA'],
 	['insource:/United States Department of Commerce|commerce.gov/i', 'PD-USGov-DOC'],
 	['insource:/Food Administration/i', 'PD-USGov-FDA'],
+	['insource:/www.nist.gov|PD-USGov-NIST/','PD-USGov-NIST'],
+	['insource:/www.nro.gov|PD-USGov-NRO/', 'PD-USGov-NRO'],
 	['insource:/Coast Guard|USCG photo/i', 'PD-USCG'],
 	['insource:/Treasury Department/', 'PD-USGov-Treasury'],
 	]
@@ -52,6 +57,8 @@ for searchstring in searchstrings:
 		print Fore.CYAN + "{}-{}".format(scount, count), Fore.GREEN + page.title()[5:-4], Fore.WHITE
 		oldhtml = page.get()
 		html = re.sub("\{\{PD-USGov\}\}", "{{" + searchstring[1] + "}}", oldhtml)
+		escss = re.escape("{{" + searchstring[1] + "}}")
+		html = re.sub(re.escape(escss +"[\n\s]*" + escss), "{{" + searchstring[1] + "}}", html)
 		if len(html)!=len(oldhtml):
 			comments = " ".join([r[3] for r in page.getVersionHistory()])
 			if re.search("PD-USGov diffusion", comments):
